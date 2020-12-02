487bfdb4923e8f09f0e27139ee20eb22238d0f26
amtoaer <amtoaer@outlook.com>
2020-12-02 23:13:55 +0800

    20/12/02

---
+++ 1.两数之和.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	m := map[int]int{}
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		}
+		m[v] = i
+	}
+	return nil
+}
+
+// @lc code=end
