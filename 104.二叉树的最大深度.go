dd8d13b0443fc4ed284a6b8c4f229d5b58f0ba8f
amtoaer <amtoaer@gmail.com>
2021-01-21 23:15:53 +0800

    21/1/21

---
+++ 104.二叉树的最大深度.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	if left > right {
+		return left + 1
+	}
+	return right + 1
+}
+
+// @lc code=end
