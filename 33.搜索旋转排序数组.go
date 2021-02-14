c6fa56d6400fd7e39fba0ee868cf87227dd9e642
amtoaer <amtoaer@gmail.com>
2021-02-14 22:40:20 +0800

    21/2/14

--- 33.搜索旋转排序数组.go
+++ 33.搜索旋转排序数组.go
@@ -7,27 +7,31 @@
 // @lc code=start
 package main
 
-import "sort"
-
 func search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
-	for left <= right {
-		mid := left + ((right - left) / 2)
-		if target == nums[mid] {
+	for left+1 < right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
 			return mid
-		} else if sort.IsSorted(sort.IntSlice(nums[left : mid+1])) {
-			if target >= nums[left] && target <= nums[mid] {
-				right = mid - 1
+		}
+		if nums[left] < nums[mid] {
+			if nums[left] <= target && target <= nums[mid] {
+				right = mid
 			} else {
-				left = mid + 1
+				left = mid
 			}
-		} else if sort.IsSorted(sort.IntSlice(nums[mid : right+1])) {
-			if target >= nums[mid] && target <= nums[right] {
-				left = mid + 1
+		} else if nums[right] > nums[mid] {
+			if nums[right] >= target && nums[mid] <= target {
+				left = mid
 			} else {
-				right = mid - 1
+				right = mid
 			}
 		}
+	}
+	if nums[left] == target {
+		return left
+	} else if nums[right] == target {
+		return right
 	}
 	return -1
 }
