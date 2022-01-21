3916e78b9da77c47fcbc28b00f2f563ba5f865ae
amtoaer <amtoaer@gmail.com>
2022-01-21 17:13:19 +0800

    22/1/21

--- 剑指offer11.go
+++ 剑指offer11.go
@@ -6,10 +6,14 @@ func minArray(numbers []int) int {
 	for low < high {
 		pivot := low + (high-low)/2
 		if numbers[pivot] < numbers[high] {
+			// 中值位于右边的递增序列，向左折半
+			// 这里不肯定pivot是否是最小值，所以查找范围内需要包括pivot
 			high = pivot
 		} else if numbers[pivot] > numbers[high] {
+			// 中值位于左边的递增序列，向右折半
 			low = pivot + 1
 		} else {
+			// 无法判断位于哪边的序列，线性查找
 			high--
 		}
 	}
